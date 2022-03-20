0ada71ac164ffa4a23f3ec6159c963218324675b
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-20 16:10:21 +0100

    import and export system practice

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import { add } from "../../utilities/calculate";
 
 const Cosmetics = () => {
-    return (
-        <div>
-            <h1>Welcome to my Cosmetics World</h1>
-        </div>
-    );
+  const first = 15;
+  const second = 45;
+  const total = add(first, second);
+
+  return (
+    <div>
+      <h1>Welcome to my Cosmetics World</h1>
+      <p>total: {total}</p>
+    </div>
+  );
 };
 
 export default Cosmetics;
---
+++ src/components/Shoes/Shoes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { add, multiply } from "../../utilities/calculate";
+const Shoes = () => {
+  const first = 20;
+  const second = 10;
+  const result = multiply(first, second);
+  const sum = add(first, second);
+  return (
+    <div>
+      <h3>This is Shoes component</h3>
+      <p>
+        Result: {result} and total: {sum}
+      </p>
+    </div>
+  );
+};
+
+export default Shoes;
