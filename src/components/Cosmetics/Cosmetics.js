d5dba2b1ae5ec1c9e888b27f749d493106f40dc2
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-20 22:02:29 +0100

    fake data generate system

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { add } from "../../utilities/calculate";
+import Cosmetic from "../Cosmetic/Cosmetic";
 
 const Cosmetics = () => {
-  const first = 15;
-  const second = 45;
-  const total = add(first, second);
+  const cosmetics = [
+    { id: 1, name: "Polish", price: 150 },
+    { id: 2, name: "Eye-Brow", price: 300 },
+    { id: 3, name: "Lip Stick", price: 500 },
+    { id: 4, name: "Body Lotion", price: 100 },
+    { id: 5, name: "Shower Gel", price: 190 },
+  ];
 
   return (
     <div>
       <h1>Welcome to my Cosmetics World</h1>
-      <p>total: {total}</p>
+
+      {cosmetics.map((cosmetic) => (
+        <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>
+      ))}
     </div>
   );
 };
