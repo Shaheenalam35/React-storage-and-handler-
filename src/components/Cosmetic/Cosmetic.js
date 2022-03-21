52635ec3f6a8a7978f8bebcfd3a6dd56fc74b57a
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-21 08:10:24 +0100

    Create event handler btn with parameter

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,21 @@
 import React from "react";
+import "./Cosmetic.css";
 
 const Cosmetic = (props) => {
   const { name, price, id } = props.cosmetic;
+  const addToCart = (id) => {
+    console.log("item Added", id);
+  };
+  const addToCartWithParam = () => addToCart(id);
   return (
-    <div>
+    <div className="product">
       <h2>Buy this:{name}</h2>
       <p>Only for:{price} </p>
-      <p>Only for:{id} </p>
+      <p>
+        <small>Its ID:{id}</small>{" "}
+      </p>
+      <button onClick={addToCartWithParam}>Add To Cart</button>
+      <button onClick={() => addToCart(id)}>Add to Cart</button>
     </div>
   );
 };
